eb908cf3096f028ccd7f0aa387595c9f18da3204
sukesh-n <sukeshtn.2@gmail.com>
2024-05-22 12:02:10 +0530

    ProjectInitialization

---
+++ QuizzAppSolution/QuizzApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizzApp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+
+    }
+}
