c3f9927a2e89e478a6099196a8ecf5dbcef503ec
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 16:32:22 +0530

    OrganizerControllerUpdate

--- QuizzAppSolution/QuizzApp/Controllers/OrganizerController.cs
+++ QuizzAppSolution/QuizzApp/Controllers/OrganizerController.cs
@@ -5,6 +5,7 @@ namespace QuizzApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles ="admin")]
     [Authorize(Roles = "organizer")]
     public class OrganizerController
     {
