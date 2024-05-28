260f983ae789f11ffedfc50286e4b17e77cc4af3
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 13:11:42 +0530

    UpdatedAddingFacilityInSolutionRepo

--- QuizzAppSolution/QuizzApp/Repositories/SolutionRepository.cs
+++ QuizzAppSolution/QuizzApp/Repositories/SolutionRepository.cs
@@ -1,13 +1,36 @@
-﻿using QuizzApp.Interfaces;
+﻿using QuizzApp.Context;
+using QuizzApp.Exceptions;
+using QuizzApp.Interfaces;
 using QuizzApp.Models;
 
 namespace QuizzApp.Repositories
 {
     public class SolutionRepository : IRepository<int, Solution>
     {
-        public Task<Solution> AddAsync(Solution entity)
+        private readonly QuizzAppContext _context;
+
+        public SolutionRepository(QuizzAppContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<Solution> AddAsync(Solution entity)
+        {
+            try
+            {
+                var Duplicate = _context.solutions.FirstOrDefault(s => s.QuestionId == entity.QuestionId);
+                if(Duplicate != null)
+                {
+                   return Duplicate;
+                }
+                var result = await _context.solutions.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return result.Entity;
+            }
+            catch
+            {
+                throw new ErrorInConnectingRepository("Unable to add Solution to Database");
+            }
         }
 
         public Task<Solution> DeleteAsync(int Key)
