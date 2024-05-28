0ba0d12305852d4439ada6e70a294e33b0f76f58
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 16:41:34 +0530

    UpdatedInterface

--- QuizzAppSolution/QuizzApp/Interfaces/IAdminInterface.cs
+++ QuizzAppSolution/QuizzApp/Interfaces/IAdminInterface.cs
@@ -1,13 +1,14 @@
 ﻿using QuizzApp.Models;
 using QuizzApp.Models.DTO;
 using QuizzApp.Models.DTO.AnalyseServiceDTO;
+using QuizzApp.Models.DTO.Test;
 
 namespace QuizzApp.Interfaces
 {
     public interface IAdminInterface : ICommonQuestionInterface
     {
         Task<bool> AddQuestionWithAnswerAsync(QuestionSolutionDTO questionSolutionDTO);
-        Task<TestDTO> ConductCommonQuizAsync(List<User> candidates, Test quiz);
+        Task<TestDTO> ConductCommonQuizAsync(List<User> candidates, QuestionSelectionDTO questionSelectionDTO);
         Task<QuestionSolutionDTO> UpdateQuestionsWithSolution(QuestionSolutionDTO questionSolutionDTO);
         Task<ResultDTO> ViewResultAnalysis();
     }
