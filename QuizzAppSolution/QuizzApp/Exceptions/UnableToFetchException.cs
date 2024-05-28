7f33d3a51334a7cdb8a9542923db110ae89996f7
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 13:07:58 +0530

    AddedNewExceptions

---
+++ QuizzAppSolution/QuizzApp/Exceptions/UnableToFetchException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace QuizzApp.Exceptions
+{
+    [Serializable]
+    internal class UnableToFetchException : Exception
+    {
+        public UnableToFetchException()
+        {
+        }
+
+        public UnableToFetchException(string? message) : base(message)
+        {
+        }
+
+        public UnableToFetchException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnableToFetchException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
