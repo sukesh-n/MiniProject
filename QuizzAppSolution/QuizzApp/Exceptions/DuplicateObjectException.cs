b6e53b79bc52aa8c9359ba3b67e2a0559508319e
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 13:17:15 +0530

    UpdatedException

---
+++ QuizzAppSolution/QuizzApp/Exceptions/DuplicateObjectException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace QuizzApp.Exceptions
+{
+    [Serializable]
+    internal class DuplicateObjectException : Exception
+    {
+        public DuplicateObjectException()
+        {
+        }
+
+        public DuplicateObjectException(string? message) : base(message)
+        {
+        }
+
+        public DuplicateObjectException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DuplicateObjectException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
