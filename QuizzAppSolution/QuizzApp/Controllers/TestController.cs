0712c3db59a545b74e0703979cd4ee83198e9f4b
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 16:31:53 +0530

    TestControllerImplementation

---
+++ QuizzAppSolution/QuizzApp/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizzApp.Interfaces.Test;
+using QuizzApp.Models.DTO;
+using QuizzApp.Models.DTO.Test;
+using QuizzApp.Exceptions;
+
+namespace QuizzApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class TestController : ControllerBase
+    {
+        private readonly ITestInterface _testInterface;
+
+        public TestController(ITestInterface testInterface)
+        {
+            _testInterface = testInterface;
+        }
+        [HttpGet("SelectCustomQuestions")]
+        public async Task<IActionResult> SelectQuestions(QuestionSelectionDTO questionSelectionDTO)
+        {
+            try
+            {
+                var QuestionSelection = await _testInterface.ChooseQuestion(questionSelectionDTO);
+                return Ok(QuestionSelection);
+            }
+            catch
+            {
+                throw new UnableToFetchException("Unable to get Questions");
+            }
+        }
+    }
+}
