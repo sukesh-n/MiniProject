421ff8ae74d9725cf02b55ab18e22a21d18d49bc
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 23:10:36 +0530

    QuestionInterfaceAsCommon

---
+++ QuizzAppSolution/QuizzApp/Interfaces/IQuestionService.cs
@@ -0,0 +1,12 @@
+﻿using QuizzApp.Models.DTO;
+
+namespace QuizzApp.Interfaces
+{
+    public interface IQuestionService
+    {
+        public Task<bool> AddQuestionWithAnswerAsync(QuestionSolutionDTO questionSolutionDTO);
+        public Task<QuestionSolutionDTO> UpdateQuestionsWithSolution(QuestionSolutionDTO questionSolutionDTO);
+
+        public Task<QuestionWithCategoryDTO> GetQuestionWithCategory(QuestionWithCategoryDTO questionWithCategoryDTO);
+    }
+}
