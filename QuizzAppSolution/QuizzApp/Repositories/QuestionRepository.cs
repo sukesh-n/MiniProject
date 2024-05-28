01b93ba5ffb21d4a7258a8fce491095e363529df
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 13:11:24 +0530

    UpdatedAddingFacilityInQuestionRepo

--- QuizzAppSolution/QuizzApp/Repositories/QuestionRepository.cs
+++ QuizzAppSolution/QuizzApp/Repositories/QuestionRepository.cs
@@ -1,13 +1,36 @@
-﻿using QuizzApp.Interfaces;
+﻿using QuizzApp.Context;
+using QuizzApp.Interfaces;
 using QuizzApp.Models;
-
+using Microsoft.EntityFrameworkCore;
+using QuizzApp.Exceptions;
 namespace QuizzApp.Repositories
 {
     public class QuestionRepository : IRepository<int, Question>
     {
-        public Task<Question> AddAsync(Question entity)
+        private readonly QuizzAppContext _context;
+
+        public QuestionRepository(QuizzAppContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<Question> AddAsync(Question entity)
+        {
+            try
+            {
+                var DuplicateCheck = _context.questions.FirstOrDefault(q => q.QuestionDescription == entity.QuestionDescription);
+                if(DuplicateCheck == null)
+                {
+                    var result = await _context.questions.AddAsync(entity);
+                    await _context.SaveChangesAsync();
+                    return result.Entity;
+                }
+                return DuplicateCheck;
+            }
+            catch
+            {
+                throw new ErrorInConnectingRepository("Unable to add Question");
+            }
         }
 
         public Task<Question> DeleteAsync(int Key)
