e3298f78c1a442e55ea6951a25a7ad8b492a7aec
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 23:10:03 +0530

    FileNameChange

---
+++ QuizzAppSolution/QuizzApp/Interfaces/IAdminService.cs
@@ -0,0 +1,14 @@
+﻿using QuizzApp.Models;
+using QuizzApp.Models.DTO;
+using QuizzApp.Models.DTO.AnalyseServiceDTO;
+using QuizzApp.Models.DTO.Test;
+
+namespace QuizzApp.Interfaces
+{
+    public interface IAdminService
+    {
+        
+        Task<TestDTO> ConductCommonQuizAsync(List<User> candidates, QuestionSelectionDTO questionSelectionDTO);       
+        Task<ResultDTO> ViewResultAnalysis();
+    }
+}
