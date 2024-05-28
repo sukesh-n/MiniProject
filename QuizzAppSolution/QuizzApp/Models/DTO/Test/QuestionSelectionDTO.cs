c54d27591e44c6ab73e529712ca7036e4ab5b564
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 16:33:33 +0530

    TestDTOImplementation

---
+++ QuizzAppSolution/QuizzApp/Models/DTO/Test/QuestionSelectionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizzApp.Models.DTO.Test
+{
+    public class QuestionSelectionDTO
+    {
+        public int NoOfQuestions { get; set; }
+        public string? MainCategory { get; set; }
+        public string? SubCategory { get; set; }
+        public int DifficultyLevel { get; set; }
+        public string? Type { get; set; }
+
+    }
+}
