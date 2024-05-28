0efb29c2546c686b25c7cee21d986c52cead8778
sukesh-n <sukeshtn.2@gmail.com>
2024-05-28 16:34:06 +0530

    InterfaceUpdated

---
+++ QuizzAppSolution/QuizzApp/Interfaces/Test/ITestInterface.cs
@@ -0,0 +1,10 @@
+﻿using QuizzApp.Models.DTO;
+using QuizzApp.Models.DTO.Test;
+
+namespace QuizzApp.Interfaces.Test
+{
+    public interface ITestInterface
+    {
+        public Task<TestDTO> ChooseQuestion(QuestionSelectionDTO questionSelectionDTO); 
+    }
+}
