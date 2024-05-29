42efeb99390559c5205d4da54f3665b17c714b29
sukesh-n <sukeshtn.2@gmail.com>
2024-05-29 18:05:54 +0530

    NoticationImplementation

---
+++ QuizzAppSolution/QuizzApp/Controllers/NotificationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QuizzApp.Exceptions;
+using QuizzApp.Interfaces.Test;
+
+namespace QuizzApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles ="admin")]
+    [Authorize(Roles = "organizer")]
+    [Authorize(Roles = "candidate")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpGet("TestNotification")]
+        public async Task<IActionResult> GetNotificationsForTest(string email,string role)
+        {
+            try
+            {
+                var checkNotification = await _notificationService.CheckNotification(email, role);
+                return Ok(checkNotification);
+            }
+            catch
+            {
+                throw new UnableToFetchException("Error while getting assignment");
+            }
+        }
+    }
+}
